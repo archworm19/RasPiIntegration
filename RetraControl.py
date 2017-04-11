8583b1259ade2d2b03b7d317b8ebaf4d4d946f9e
archworm19 <ztcecere@gmail.com>
2017-04-11 03:46:31 -0700

    intensity control added

--- RetraControl.py
+++ RetraControl.py
@@ -4,6 +4,7 @@ Communicate Serial to Retra
 
 '''
 import serial
+import time
 
 
 class RetraControl:
@@ -25,7 +26,7 @@ class RetraControl:
 
         # TODO: intensity command prefix and postfix:
         this.prefix_str = str(bytearray([83, 24, 3, 8]))
-        this.cur_byte = bytearray([240, 0]) 
+        this.cur_byte = 240
         this.postfix_str = str(bytearray([80]))
 
 
@@ -37,8 +38,33 @@ class RetraControl:
         this.port.write(this.red_off_str)
 
 
-    # TODO: set intensity
-    # Takes in 2 bytes for intensity 
+    # set intensity
+    # Takes in integer intensity value
+    # max = 4095
     def set_intensity(self, intense):
- 
+        # 1st: invert
+        inv_intense = 4095 - intense 
+
+        # translate inverse intensity value into two bytes
+        large_byte = int(inv_intense / 255)
+        little_byte = int(inv_intense - (255 * large_byte))
+
+        write_intense = str(byte_array([large_byte, little_byte]))
+
+        this.port.write(this.prefix_str)
+        this.port.write(write_intense)
+        this.port.write(this.postfix_str)
+
+
+# testing:
+if(__name__ == '__main__'):
+    rc = RetraControl('/dev/ttyUSB0')
+    rc.red_on()
+    time.sleep(1)
+    rc.set_intensity(200)
+    time.sleep(1)
+    rc.set_intensity(800)
+    time.sleep(1)
+    rc.red_off() 
+
 

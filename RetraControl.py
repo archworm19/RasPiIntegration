590fb32188ebf3c59a12a116db997074f39659a3
archworm19 <ztcecere@gmail.com>
2017-04-14 23:28:03 +0000

    working retra control

--- RetraControl.py
+++ RetraControl.py
@@ -12,30 +12,29 @@ class RetraControl:
     # initialize port and useful commands 
     def __init__(self, port_str):
         # initialize port:
-        this.port = serial.Serial(port_str) # 8N1 standard
+        self.port = serial.Serial(port_str) # 8N1 standard
 
         # initialize communication:
         i1 = bytearray([87, 2, 170, 80])
         i2 = bytearray([87, 3, 170, 80])
-        this.port.write(str(i1))
-        this.port.write(str(i2))
+        self.port.write(str(i1))
+        self.port.write(str(i2))
 
         # initialize useful strings:
-        this.red_on_str = str(bytearray([79, 254, 80]))
-        this.red_off_str = str(bytearray([79, 255, 80]))
+        self.red_on_str = str(bytearray([79, 254, 80]))
+        self.red_off_str = str(bytearray([79, 255, 80]))
 
         # TODO: intensity command prefix and postfix:
-        this.prefix_str = str(bytearray([83, 24, 3, 8]))
-        this.cur_byte = 240
-        this.postfix_str = str(bytearray([80]))
+        self.prefix_byte = bytearray([83, 24, 3, 7])
+        self.postfix_byte = bytearray([80])
 
 
     def red_on(self):
-        this.port.write(this.red_on_str)
+        self.port.write(self.red_on_str)
 
 
     def red_off(self):
-        this.port.write(this.red_off_str)
+        self.port.write(self.red_off_str)
 
 
     # set intensity
@@ -46,25 +45,35 @@ class RetraControl:
         inv_intense = 4095 - intense 
 
         # translate inverse intensity value into two bytes
-        large_byte = int(inv_intense / 255)
-        little_byte = int(inv_intense - (255 * large_byte))
+        large_byte = int(inv_intense / 256)
+        little_byte = int(inv_intense - (256 * large_byte))
 
-        write_intense = str(byte_array([large_byte, little_byte]))
+        print(large_byte)
+        print(little_byte) 
 
-        this.port.write(this.prefix_str)
-        this.port.write(write_intense)
-        this.port.write(this.postfix_str)
+        write_intense = bytearray([240+large_byte, little_byte])
 
+        write_str = str(self.prefix_byte + write_intense + self.postfix_byte)
+
+        '''
+        test_str = str(bytearray([83, 24, 3, 7, 255, 255, 80]))
+        self.port.write(test_str)
+        '''
+        
+        self.port.write(write_str)
+        
 
 # testing:
 if(__name__ == '__main__'):
     rc = RetraControl('/dev/ttyUSB0')
     rc.red_on()
-    time.sleep(1)
+    time.sleep(2)
+    rc.set_intensity(0)
+    time.sleep(2)
     rc.set_intensity(200)
-    time.sleep(1)
-    rc.set_intensity(800)
-    time.sleep(1)
+    time.sleep(2)
+    rc.set_intensity(1000)
+    time.sleep(2)
     rc.red_off() 
 
 
